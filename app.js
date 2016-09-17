1a97d77d9c8bdc874549113fe13e7502e00443f9
vainotuisk <vainotuisk@gmail.com>
2016-09-17 21:47:31 +0300

    Pooleli

---
+++ app.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+  angular.module('LunchCheck',[])
+  .controller('LunchController',LunchController);
+  LunchController.$inject =[$scope];
+  function LunchController($scope) {
+    $scope.nimi='vaino';
+    $scope.checkLunch= function () {
+      
+    }
+  }
+}
+
+)();
