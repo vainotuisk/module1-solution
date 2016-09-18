d2eb1f4e3e176dbd5d1742a8a30ba68ff4f3b29f
vainotuisk <vainotuisk@gmail.com>
2016-09-18 15:45:27 +0300

    final

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
 (function () {
-  'use strict';
-  angular.module('LunchCheck',[])
-  .controller('LunchController',LunchController);
-  LunchController.$inject =[$scope];
-  function LunchController($scope) {
-    $scope.nimi='vaino';
-    $scope.items='items';
-    $scope.numberOfItems = 0;
-    $scope.checkLunch= function () {
-      return $scope.items;
-      console.log('number: '+$scope.nimi);
-//return $scope.numberOfItems;
-    };
-  }
+'use strict';
 
+angular.module('LunchCheck', [])
+.controller('LunchController', LunchController);
+
+LunchController.$inject = ['$scope'];
+function LunchController($scope) {
+  $scope.items = "";
+  $scope.numberOfItems = 0;
+  $scope.customMessage = '';
+  $scope.countItems = function () {
+  var totalLunches = checkLunch($scope.items);
+  // switch (totalLunches) {
+  //   case false:
+  //     $scope.customMessage='Please enter data first'
+  //     break;
+  //     case :
+  //       $scope.customMessage='Please enter data first'
+  //       break;
+  //   default:
+  //
+  // }
+  if (totalLunches == false)
+    {$scope.customMessage='Please enter data first'}
+  else if (totalLunches > 3) {
+    {$scope.customMessage='Too much!'}
+  }
+  else
+    {$scope.customMessage='Enjoy!'}
+  $scope.numberOfItems = totalLunches;
+  };
+  function checkLunch(items_string) {
+    if (!items_string) {
+        return false;
+    }
+    else {
+        var items = items_string.split(',');
+            console.log('praegu on '+ items+ 'ja elemente on '+ items.length);
+
+        return items.length;
+    }
+};
+
+}
 
 })();
